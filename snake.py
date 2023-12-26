518e899ab96d950909368a8cc97aa6c1973bdbbd
Tr0ph1c <ahmmahmouda767@gmail.com>
2023-12-26 16:52:24 +0200

    Started Challenge: Create Snake Game
    made basic outline of the system

---
+++ snake.py
@@ -0,0 +1,16 @@
+# Trying to implement the Snake game on python
+#
+#
+#
+#
+#
+#
+
+def main:
+    #Render Frame
+    #Get Input
+    #Process game state
+    #loop
+
+if __name__ == "__main__":
+    main()
